8d5381838ddf487cfe67bc254c6d82bf87d15f61
nikzart <32996495+nikzart@users.noreply.github.com>
2021-03-19 21:13:12 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,6 @@ module.exports = {
     log_date_format: 'HH:mm:ss',
     watch: true,
     ignore_watch : [".git", "temp", "data", "node_modules"],
-    max_memory_restart: '2G',
+    max_memory_restart: '750M',
   }]
 };
